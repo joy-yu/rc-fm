397e92c3f6ed0ec5649f85cfeebee17fa0321fd5
joyu <775154773@qq.com>
2017-03-04 17:10:09 +0800

    update

--- src/RunType.js
+++ src/RunType.js
@@ -5,7 +5,7 @@ export default class RunType extends Component {
 
   render(){
     return(
-      <a id="runType" onClick={this.props.changeRunType} >
+      <a id="runType" onClick={this.props.changeRunType.bind(null, this.refs['typeIcon'])} >
         <i className="i-loop" ref="typeIcon"></i>
       </a>
     );
